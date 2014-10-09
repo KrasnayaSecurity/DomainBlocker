0f3f24556630e6e5bdf7b742bdd79e7367890576
sasha <spavelovich@lexipedium.org>
2014-10-09 13:39:13 -0400

    Updating program

--- blocker.c
+++ blocker.c
@@ -12,6 +12,7 @@ int main(int argc, char* argv[])
     char h_text[1024*500];
     char* h_bs;
     char* h_as;
+    char* new_hosts;
     int bf_size;
     int h_size;
 
@@ -66,15 +67,30 @@ int main(int argc, char* argv[])
         printf("The hosts file does not include my blocked domains.\n");
         strcat(h_text, "\n\n\n");
         strcat(h_text, HOSTS_MARKER);
+        strcat(h_text, "|");
         printf("I have added the hosts file seperator.\n%s\n", h_text);
     }
     //printf("\n%s\n", h_text);
 
     // Seperate the manual and automatic hosts file parts
-    h_bs = strtok(h_text, HOSTS_MARKER);
-    strcat(h_text, "\n\n\n");
-    strcat(h_text, HOSTS_MARKER);
-    printf("System and user defined hosts file:\n%s\n", h_text);
+    h_bs = strtok(h_text, "|");
+    strcat(h_bs, "|\n");
+    printf("System and user defined hosts file:\n%s\n", h_bs);
+
+    // Print the system and custom hosts file section to .hosts
+    fprintf(hosts_tmp, "%s", h_bs);
+    // Transfer hosts pre seperation to new hosts variable
+    new_hosts = h_bs;
+    strcat(new_hosts, "\n");
+    strcat(new_hosts, request);
+    printf("Final content for hosts file:\n%s\n", new_hosts);
+
+    // Closing and reopening the hosts file in a different mode to write to it without appending
+    fclose(hosts);
+    hosts = fopen("hosts", "w");
+
+    // Write the content to the hosts file
+    fwrite(new_hosts, 1, 1024, hosts);
 
     fclose(block_file);
     fclose(hosts);
